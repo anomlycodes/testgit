67cc6b556aa7571235776fe1213343f68625d618
anomlycodes <127826241+anomlycodes@users.noreply.github.com>
2023-03-14 12:17:08 +0530

    Update app.java

--- src/main/java/app.java
+++ src/main/java/app.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class app {
     public static void main(String[] args) {
         System.out.println("Hello changed");
+        //update project
         Scanner sc=new Scanner(System.in);
     }
 }
