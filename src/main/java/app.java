530b7d380091c6a7c69febd51927d913ff71816f
anomlycodes <workforflint@gmail.com>
2023-03-14 12:14:23 +0530

    Third Commit

--- src/main/java/app.java
+++ src/main/java/app.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
+
 public class app {
     public static void main(String[] args) {
         System.out.println("Hello master");
+        Scanner sc=new Scanner(System.in);
     }
 }
